6fa2297f54de8bb8e248bbde382f4da3f00fa0f6
Lucien <lucie_linux@protonmail.com>
2022-09-13 20:40:03 -0300

    Add svelte template + prettier + husky

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command, mode }) => {
+    // const isDevMode = mode.toLowerCase() === 'development';
+
+    return {
+        plugins: [svelte()],
+        server: {
+            host: '127.0.0.1',
+            port: 3000,
+        },
+    };
+});
