42e8189835bcc9963d90fa1c7accbe81d5c8c16d
Lucien <lucie_linux@protonmail.com>
2022-09-14 20:46:36 -0300

    fast_commit at 20220914_204636

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
+import { existsSync } from 'fs';
 
 const baseDir = dirname(fileURLToPath(import.meta.url));
 const spaDir = join(baseDir, 'dist');
+
+if (!existsSync(spaDir)) {
+    console.error('Error: SPA folder does not exist. Did you forget to run the build script?');
+    process.exit(1);
+}
 
 dotenv.config();
 
