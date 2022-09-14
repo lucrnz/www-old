08407c3ef0175f01644faee53586efd99cdbb1e3
Lucien <lucie_linux@protonmail.com>
2022-09-13 21:07:05 -0300

    Added prettier-plugin-svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,17 @@
 <script>
-  import Counter from './lib/Counter.svelte'
+    import Counter from './lib/Counter.svelte';
 </script>
 
 <main>
-  <h1>Vite + Svelte</h1>
+    <h1>Lucien's Website!</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+    <div class="card">
+        <Counter />
+    </div>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+    h1 {
+        font-size: 1.5rem;
+    }
 </style>
--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-    // const isDevMode = mode.toLowerCase() === 'development';
+    const isDevMode = mode.toLowerCase() === 'development';
 
     return {
         plugins: [svelte()],
         server: {
-            host: '127.0.0.1',
+            host: isDevMode ? '127.0.0.1' : '::',
             port: 3000,
         },
     };
