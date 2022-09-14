cc051aaf076b056f5b771aaf941e439bae008e5b
Lucien <lucie_linux@protonmail.com>
2022-09-13 23:03:05 -0300

    Renamed App to MainPage

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import './app.css';
-import App from './App.svelte';
+import './main.css';
+import MainPage from './MainPage.svelte';
 
-const app = new App({
+const app = new MainPage({
     target: document.getElementById('app'),
 });
 
