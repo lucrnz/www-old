174befafaa587b2f8bca88639d96df0c76fedbba
Luc/Lucie/Lucien lucdev.net <lucie_linux@protonmail.com>
2022-10-04 05:15:06 -0300

    Public release: Add license, edit README

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-export PATH=/home/lucie/.local/share/nvm/versions/node/v18.9.1/bin/:/home/lucie/.local/share/pnpm:/home/lucie/.local/share/dotnet:$PATH
+export PATH=$HOME/.local/share/nvm/versions/node/v18.9.1/bin/:$HOME/.local/share/pnpm:$HOME/.local/share/dotnet:$PATH
 pm2 start pm2.json
